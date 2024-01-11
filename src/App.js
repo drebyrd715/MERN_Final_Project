f3b857dc8d297a18d72d9a471467360c75a9982d
mouloud <mouloud619@gmail.com>
2024-01-11 14:31:29 -0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,30 +1,31 @@
+
 import "./App.css";
 import { useState } from "react";
 import { Routes, Route } from "react-router-dom";
 import { getUser } from "./utilities/users-service";
-//------components
 import NewOrderPage from "./components/NewOrderPage";
 import AuthPage from "./components/AuthPage";
 import OrderHistoryPage from "./components/OrderHistoryPage";
-import NavBar from "./components/NavBar";
+import Navbar from "./components/Navbar";
+
 
 function App() {
   const [user, setUser] = useState(getUser());
-
   return (
-    <main className="App">
+    <div className="App">
+
       {user ? (
         <>
-          <NavBar user={user} setUser={setUser} />
+          <Navbar user={user} setUser={setUser} />
           <Routes>
             <Route path="/orders/new" element={<NewOrderPage />} />
-            <Route path="/orders" element={<OrderHistoryPage />} />
+            <Route path="/orders" element={<OrderHistoryPage/>} />
           </Routes>
         </>
       ) : (
-        <AuthPage />
+        <AuthPage user={user} setUser={setUser}/>
       )}
-    </main>
+    </div>
   );
 }
 
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,32 +1,30 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import * as userService from "../utilities/users-service";
+import * as userService from '../utilities/users-service'
+function Navbar({user ,setUser}) {
 
-function NavBar({ user, setUser }) {
   function handleLogOut() {
     // Delegate to the users-service
     userService.logOut();
     // Update state will also cause a re-render
     setUser(null);
   }
-
   return (
-    <nav>
-      <div style={{ display: "flex", justifyContent: "space-around" }}>
-        {/* <h5>Hello {user.name}</h5>
-        <h5>Logged in with: {user.email}</h5>
-        <Link to=""><button>logOut</button></Link> */}
-        &nbsp;&nbsp;<span>Welcome, {user.name}</span>
-        &nbsp;&nbsp;
-        <Link to="" onClick={handleLogOut}>
-          Log Out
-        </Link>
+    <>
+      <nav style={{ justifyContent: "space-evenly", display: "flex" }}>
+
+      <div style={{justifyContent:'space-around'}}>
+        <p style={{margin:'1em'}}>Welcome {user.name}, </p><hr/>
+        <p style={{margin:'1em'}}> Logged In : {user.email}</p>
+        <Link to="" onClick={handleLogOut}><button>Log-Out</button></Link>
       </div>
-      <Link to="/orders">Order History</Link>
-      &nbsp; | &nbsp;
-      <Link to="/orders/new">New Order</Link>
-    </nav>
+
+        <Link to="/orders">Order History</Link>
+
+        <Link to="/orders/new">New Order</Link>
+      </nav>
+    </>
   );
 }
 
-export default NavBar;
+export default Navbar;
