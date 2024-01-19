edb6ca6eb3bbb0fd48ac3ceb2227d2b8fcf75c3a
drebyrd715 <drebyrd715@gmail.com>
2024-01-19 15:08:18 -0500

    trying to add api

---
+++ src/components/Nav.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+function Nav() {
+  return (
+    <div className="nav">
+      <Link to="/">
+        <h4>Home</h4>
+      </Link>
+
+      <Link to="/orders/new">
+        <h4> Orders </h4>
+      </Link>
+
+      <Link to="/PokemonPics">
+        <h4>Pokemon</h4>
+      </Link>
+    </div>
+  );
+}
+
+export default Nav;
--- src/pages/App/App.js
+++ src/pages/App/App.js
@@ -1,28 +1,40 @@
-import React, { useState } from 'react';
+import React, { useState, createContext } from 'react';
 import { Routes, Route, Navigate } from 'react-router-dom';
 import styles from './App.module.css';
 import { getUser } from '../../utilities/users-service';
+import Home from '../Home';
+// import './App.css';
+import Nav from '../../components/Nav';
 import AuthPage from '../AuthPage/AuthPage';
 import NewOrderPage from '../NewOrderPage/NewOrderPage';
 import OrderHistoryPage from '../OrderHistoryPage/OrderHistoryPage';
+import PokemonPics from '../../components/PokemonPics';
+export const Context = createContext();
 
 export default function App() {
   const [user, setUser] = useState(getUser());
+  const [allPokemon, setAllPokemon] = useState([]);
+  
   return (
+    <Context.Provider value ={{allPokemon, setAllPokemon}}>
     <main className={styles.App}>
       { user ?
         <>
+        <Nav />
           <Routes>
+          <Route path="/" element={<Home/>} />
             {/* client-side route that renders the component instance if the path matches the url in the address bar */}
             <Route path="/orders/new" element={<NewOrderPage user={user} setUser={setUser} />} />
             <Route path="/orders" element={<OrderHistoryPage user={user} setUser={setUser} />} />
             {/* redirect to /orders/new if path in address bar hasn't matched a <Route> above */}
             <Route path="/*" element={<Navigate to="/orders/new" />} />
+            <Route path="/pokemonpics" element={<PokemonPics/>} />
           </Routes>
         </>
         :
         <AuthPage setUser={setUser} />
       }
     </main>
+    </Context.Provider>
   );
 }
