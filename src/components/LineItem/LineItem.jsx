28af9fb283c840927b0a515f849e0344c759e68e
drebyrd715 <drebyrd715@gmail.com>
2024-01-19 13:06:35 -0500

    trying to work on css

--- src/components/LineItem/LineItem.jsx
+++ src/components/LineItem/LineItem.jsx
@@ -3,7 +3,8 @@ import styles from './LineItem.module.css';
 export default function LineItem({ lineItem, isPaid, handleChangeQty }) {
 return (
   <div className={styles.LineItem}>
-    <div className="flex-ctr-ctr">{lineItem.item.image}</div>
+    <div className="flex-ctr-ctr">  
+     <img src={lineItem.item.image} /></div>
     <div className="flex-ctr-ctr flex-col">
       <span className="align-ctr">{lineItem.item.name}</span>
       <span>{lineItem.item.price.toFixed(2)}</span>
