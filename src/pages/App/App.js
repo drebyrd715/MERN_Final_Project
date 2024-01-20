4eaf451ff728e8fb3f2ce3e857fb8f8594b4db42
drebyrd715 <drebyrd715@gmail.com>
2024-01-19 20:11:47 -0500

    apiworks

--- src/pages/App/App.js
+++ src/pages/App/App.js
@@ -2,19 +2,18 @@ import React, { useState, createContext } from 'react';
 import { Routes, Route, Navigate } from 'react-router-dom';
 import styles from './App.module.css';
 import { getUser } from '../../utilities/users-service';
-import Home from '../Home';
-// import './App.css';
+import Home from '../HomePage';
 import Nav from '../../components/Nav';
 import AuthPage from '../AuthPage/AuthPage';
 import NewOrderPage from '../NewOrderPage/NewOrderPage';
 import OrderHistoryPage from '../OrderHistoryPage/OrderHistoryPage';
-import PokemonPics from '../../components/PokemonPics';
+import PokemonPics from '../PokemonPage';
 export const Context = createContext();
 
 export default function App() {
   const [user, setUser] = useState(getUser());
   const [allPokemon, setAllPokemon] = useState([]);
-  
+
   return (
     <Context.Provider value ={{allPokemon, setAllPokemon}}>
     <main className={styles.App}>
